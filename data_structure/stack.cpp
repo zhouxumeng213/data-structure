9dd1fd65c584422baa3eadf5d75b81dcadc3a7f6
zhouxumeng213 <18710775233@163.com>
2016-08-05 10:20:47 +0800

    更改stack的代码，更容易理解

--- data_structure/stack.cpp
+++ data_structure/stack.cpp
@@ -1,60 +1,78 @@
 #include<iostream>
 using namespace std;
+#include<assert.h>
 template<class T>
-class DoubleStack
+class Stack
 {
 public:
-	DoubleStack(T* commenArray, size_t topIndex = 0)
-		:_array(commenArray)
-		, _capacity(3)
-		, _topIndex(topIndex)
+	Stack()
+		:_capacity(3)
+		, _size(0)
+		, _array(new T[_capacity])
 	{}
+	~Stack()
+	{
+		if (_array)
+			delete[] _array;
+	}
 	void CheckCapacity()
 	{
-		if (_topIndex == _capacity)
+		if (_size == _capacity)
 		{
 			_capacity = 2 * _capacity + 3;
 			T* tmp = new T[_capacity];
-			memcpy(tmp, _array, sizeof(T)*(_topIndex));
-			delete _array;
+			//当是自定义类型时，注意深浅拷贝问题
+			//memcpy(tmp, _array, sizeof(T)*(_topIndex+1));
+			for (size_t i = 0; i < _size; i++)
+				tmp[i] = _array[i];
+			delete[] _array;
 			_array = tmp;
 		}
 	}
 	bool IsEmpty()
 	{
-		return (_topIndex == 0) || (_topIndex == 1);
+		return _size == 0;
 	}
 	void Push(const T& x)
 	{
 		CheckCapacity();
-		_array[_topIndex] = x;
-		_topIndex += 2;
+		_array[_size++] = x;
 	}
 	void Pop()
 	{
-		--_topIndex;
+		assert(_size > 0);
+		--_size;
 	}
-	T& Top()
+	const T& Top()
 	{
-		return _array[_topIndex];
+		return _array[_size - 1];
+	}
+	size_t Size()
+	{
+		return _size;
+	}
+	void Print()
+	{
+		for (size_t i = 0; i < _size; i++)
+			cout << _array[i] << " ";
+		cout << endl;
 	}
 private:
-	T* & _array;
-	size_t _capacity;
-	size_t _topIndex;
+	size_t _capacity;	//容量
+	size_t _size;		//数据的个数
+	T*  _array;		//指向数据块的指针
 };
 void Test()
 {
-	int* commonArray1 = 0;
-	DoubleStack<int> s1(commonArray1, 0);
-	DoubleStack<int> s2(commonArray1, 1);
+	Stack<int> s1;
 	s1.Push(1);
-	s2.Push(2);
+	s1.Push(2);
 	s1.Push(3);
-	s2.Push(4);
-	s1.Push(5);
-	s2.Push(6);
-
+	s1.Push(4);
+	s1.Print();
+	s1.Pop();
+	s1.Pop();
+	s1.Print();
 }
 int main()
 {
